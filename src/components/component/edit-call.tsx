29faa539a3f51f781beb377b1cb44fced2e2a295
Yanga <yangagovuza@gmail.com>
2024-08-26 10:39:58 +0200

    no quotes

--- src/components/component/edit-call.tsx
+++ src/components/component/edit-call.tsx
@@ -285,7 +285,7 @@ export function EditCall({ closeEdit, data }: Props) {
                 { value: phonenumber, message: "Please enter the phone number." },
                 { value: clientName, message: "Please enter the client name." },
                 { value: emailAdd, message: "Please enter the clients email address." },
-                { value: anydesk, message: 'Please enter the client&rsquo;s Anydesk.' }, // Escaped single quote
+                { value: anydesk, message: 'Please enter the clients Anydesk.' }, // Escaped single quote
                 { value: type, message: "Please select a call type." },
                 { value: employee, message: "Please select an employee." },
                 { value: comments, message: "Please enter any comments relevant to the Task/Error." },
